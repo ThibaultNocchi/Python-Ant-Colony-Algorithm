338f012872bda46293d20b9cdd94161a75f1db3a
Thibault Nocchi <thibaultnocchi@gmail.com>
2018-11-23 21:34:04 +0100

    Added the same command line parameters to the GUI.

--- GUI.py
+++ GUI.py
@@ -2,6 +2,7 @@ import Model
 import pygame
 from pygame.locals import *
 from time import sleep
+import argparse
 
 class Screen: 	# Screen class to interact with the screen
 
@@ -129,10 +130,46 @@ class Screen: 	# Screen class to interact with the screen
 				pygame.quit() 	# We stop the game
 
 
-width = 10
-scoutAnts = width
-workerAnts = 10*width
-decreaseRate = 0.7
+def check_positive(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+         raise argparse.ArgumentTypeError("argument needs to be positive")
+    return ivalue
+
+
+def check_0_1(value):
+	fvalue = float(value)
+	if fvalue < 0 or fvalue > 1:
+		raise argparse.ArgumentTypeError("argument needs to be between 0 and 1")
+	return fvalue
+
+
+parser = argparse.ArgumentParser("Console.py", description="Runs a simulation of the algorithm and displays the resulted path.")
+parser.add_argument("--width", help="Side size of the world. (Default: 10)", type=check_positive)
+parser.add_argument("-s", "--scouts", help="Number of scout ants. (Default: width)", type=check_positive)
+parser.add_argument("-w", "--workers", help="Number of worker ants. (Default: 10*width)", type=check_positive)
+parser.add_argument("-r", "--rate", help="Pheromones decrease rate between 0 and 1. (Default: 0.7)", type=check_0_1)
+args = parser.parse_args()
+
+if args.width:
+	width = args.width
+else:
+	width = 10
+
+if args.scouts:
+	scoutAnts = args.scouts
+else:
+	scoutAnts = width
+
+if args.workers:
+	workerAnts = args.workers
+else:
+	workerAnts = 10*width
+
+if(args.rate):
+	decreaseRate = args.rate
+else:
+	decreaseRate = 0.7
 
 world = Model.World(width, scoutAnts, workerAnts, decreaseRate)
 screen = Screen(world.width, world.anthill, world.food)
